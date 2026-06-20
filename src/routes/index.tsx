5954d898619ddb3508a572523182b3e97114024a
drwstnmtz <drwstnmtz@gmail.com>
2026-06-19 20:15:21 -0400

    Update index.tsx
    added reviews to menu bar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -64,6 +64,7 @@ const NAV = [
   { label: "Home", href: "#home" },
   { label: "Services", href: "#services" },
   { label: "About", href: "#about" },
+  { label: "Reviews", href: "#reviews" },
   { label: "FAQ", href: "#faq" },
   { label: "Contact", href: "#contact" },
 ];
